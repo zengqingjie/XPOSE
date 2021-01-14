1dc408988d4b624fd592dbcf9207a64bc2da847c
chenchao <935626020@qq.com>
2021-01-14 14:34:14 +0800

    fix: 更改this指向

--- src/utils/custom_active.js
+++ src/utils/custom_active.js
@@ -8,13 +8,13 @@ export const customActive = {
       scroll: false,
       ...opt,
       start: function (event, ui) {
-        callBack && callBack.start && callBack.start(event, ui);
+        callBack && callBack.start && callBack.start.call(this, event, ui);
       },
       drag: function (event, ui) {
-        callBack && callBack.drag && callBack.drag(event, ui);
+        callBack && callBack.drag && callBack.drag.call(this, event, ui);
       },
       stop: function (event, ui) {
-        callBack && callBack.stop && callBack.stop(event, ui);
+        callBack && callBack.stop && callBack.stop.call(this, event, ui);
       }
     });
   },
@@ -23,19 +23,19 @@ export const customActive = {
     $(target).droppable({
       ...opt,
       activate: function (event, ui) {
-        callBack && callBack.activate && callBack.activate(event, ui);
+        callBack && callBack.activate && callBack.activate.call(this, event, ui);
       },
       deactivate: function (event, ui) {
-        callBack && callBack.deactivate && callBack.deactivate(event, ui);
+        callBack && callBack.deactivate && callBack.deactivate.call(this, event, ui);
       },
       out: function (event, ui) {
-        callBack && callBack.out && callBack.out(event, ui);
+        callBack && callBack.out && callBack.out.call(this, event, ui);
       },
       over: function (event, ui) {
-        callBack && callBack.over && callBack.over(event, ui);
+        callBack && callBack.over && callBack.over.call(this, event, ui);
       },
       drop: function (event, ui) {
-        callBack && callBack.drop && callBack.drop(event, ui);
+        callBack && callBack.drop && callBack.drop.call(this, event, ui);
       }
     });
   },
@@ -44,16 +44,16 @@ export const customActive = {
     $(target).droppable({
       ...opt,
       create: function (event, ui) {
-        callBack && callBack.create && callBack.create(event, ui);
+        callBack && callBack.create && callBack.create.call(this, event, ui);
       },
       resize: function (event, ui) {
-        callBack && callBack.resize && callBack.resize(event, ui);
+        callBack && callBack.resize && callBack.resize.call(this, event, ui);
       },
       start: function (event, ui) {
-        callBack && callBack.start && callBack.start(event, ui);
+        callBack && callBack.start && callBack.start.call(this, event, ui);
       },
       stop: function (event, ui) {
-        callBack && callBack.stop && callBack.stop(event, ui);
+        callBack && callBack.stop && callBack.stop.call(this, event, ui);
       },
     });
   }

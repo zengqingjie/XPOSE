2eb47c53b632082b442bb9d7871e791ced3a59e3
chenchao <935626020@qq.com>
2021-01-15 16:06:38 +0800

    fix: 缩放移动比例

--- src/components/widget/display.model.js
+++ src/components/widget/display.model.js
@@ -15,7 +15,29 @@ export class Display extends Widget {
         yRadio: 1
       },
       col: 1,
-      row: 1
+      row: 1,
+      positionZoom: {
+        wZoom: 0,
+        hZoom: 0,
+      }
     }, config.customFeature)
+  }
+
+  setPositionZoom(content) {
+    let { left, top } = this.position;
+    let { w, h } = content;
+    Object.assign(this.customFeature.positionZoom, {
+      wZoom: (left / w).toFixed(4),
+      hZoom: (top / h).toFixed(4)
+    })
+  }
+
+  resetPosition(content) {
+    let { w, h } = content;
+    let { wZoom, hZoom } = this.customFeature.positionZoom;
+    Object.assign(this.position, {
+      left: (w * wZoom).toFixed(4),
+      top: (h * hZoom).toFixed(4)
+    })
   }
 }
